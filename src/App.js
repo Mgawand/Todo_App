d68e4cf027d70e8427c1033fa541197e18d89b68
Mamata Gawand <mamata.g@rejolut.com>
2022-07-29 13:15:57 +0530

    case sensitive

--- src/App.js
+++ src/App.js
@@ -1,68 +1,62 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
-import { Footer } from "./components/Footer";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Header from './components/Header';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import { Footer } from './components/Footer';
 
 const data = [
-  {
-    id: 1,
-    content: "Complete udemy js course",
-    completed: false,
-  },
-  {
-    id: 2,
-    content: "Completed udacity course",
-    completed: true,
-  },
+   {
+      id: 1,
+      content: 'Complete udemy js course',
+      completed: false
+   },
+   {
+      id: 2,
+      content: 'Completed udacity course',
+      completed: true
+   }
 ];
 
-// data.map((item, index) => {
-//   if(!dataOptions.some(({value,label}) =>  label == content)){
-//     dataOptions.push({value: index, label:content})
-//   }
-// });
-
 function App() {
-  const [todos, setTodos] = useState(data);
-  const [filter, setFilter] = useState("all");
-  const [filteredTodos, setFilteredTodos] = useState(todos);
-  const [themeLight, setThemeLight] = useState(true);
+   const [todos, setTodos] = useState(data);
+   const [filter, setFilter] = useState('all');
+   const [filteredTodos, setFilteredTodos] = useState(todos);
+   const [themeLight, setThemeLight] = useState(true);
 
-  const themeClass = themeLight ? "light" : "dark";
+   const themeClass = themeLight ? 'light' : 'dark';
 
-  useEffect(() => {
-    const handleFilter = () => {
-      if (filter === "active") {
-        return setFilteredTodos(todos.filter((todo) => !todo.completed));
-      } else if (filter === "completed") {
-        return setFilteredTodos(todos.filter((todo) => todo.completed));
-      } else {
-        return setFilteredTodos(todos);
-      }
-    };
-    handleFilter();
-  }, [todos, filter]);
+   useEffect(() => {
+      const handleFilter = () => {
+         if (filter === 'active') {
+            return setFilteredTodos(todos.filter((todo) => !todo.completed));
+         } else if (filter === 'completed') {
+            return setFilteredTodos(todos.filter((todo) => todo.completed));
+         } else {
+            return setFilteredTodos(todos);
+         }
+      };
+      handleFilter();
+   }, [todos, filter]);
 
-  return (
-    <div className={`wrapper ${themeClass}`}>
-      <div className="container">
-        <Header themeLight={themeLight} setThemeLight={setThemeLight} />
-        <main>
-          <TodoForm todos={todos} setTodos={setTodos} />
-          <TodoList
-            todos={todos}
-            setTodos={setTodos}
-            filteredTodos={filteredTodos}
-            filter={filter}
-            setFilter={setFilter}
-          />
-        </main>
-        <Footer />
+   return (
+      <div className={`wrapper ${themeClass}`}>
+         <div className="container">
+            <Header themeLight={themeLight} setThemeLight={setThemeLight} />
+            <main>
+               <TodoForm todos={todos} setTodos={setTodos} />
+               <TodoList
+                  todos={todos}
+                  setTodos={setTodos}
+                  filteredTodos={filteredTodos}
+                  filter={filter}
+                  setFilter={setFilter}
+               />
+            </main>
+            <Footer />
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default App;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,52 +1,54 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const idGenerate = (array) => {
-  const ids = array.map((item) => item.id);
-  return Math.max(...ids) + 1;
+   const ids = array.map((item) => item.id);
+   return Math.max(...ids) + 1;
 };
 
 const TodoForm = ({ todos, setTodos }) => {
-  const [todoInput, setTodoInput] = useState("");
+   const [todoInput, setTodoInput] = useState('');
 
-  const handleChange = (event) => {
-    setTodoInput(event.target.value);
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const existing = todos.find((todo) => todo.content === todoInput);
-    if (existing) {
-      alert("already exists");
-    } else if (todoInput) {
-      const newTodo = {
-        id: idGenerate(todos),
-        content: todoInput.trim(),
-        completed: false,
-      };
-      setTodos([newTodo, ...todos]);
-      setTodoInput("");
-    }
-  };
-  return (
-    <div className="form-container">
-      <div className="checkbox-border">
-        <span className="checkbox"></span>
+   const handleChange = (event) => {
+      setTodoInput(event.target.value);
+   };
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      const existing = todos.find(
+         (todo) => todo.content.toLowerCase() === todoInput.toLowerCase()
+      );
+      if (existing) {
+         alert('already exists');
+      } else if (todoInput) {
+         const newTodo = {
+            id: idGenerate(todos),
+            content: todoInput.trim(),
+            completed: false
+         };
+         setTodos([newTodo, ...todos]);
+         setTodoInput('');
+      }
+   };
+   return (
+      <div className="form-container">
+         <div className="checkbox-border">
+            <span className="checkbox"></span>
+         </div>
+         <form onSubmit={handleSubmit}>
+            <input
+               className="todo-input"
+               id="todoInput"
+               type="text"
+               name="todo-input"
+               placeholder="Create a new todo..."
+               value={todoInput}
+               onChange={handleChange}
+            />
+            <button id="submitNewTodo" type="submit">
+               Add Todo
+            </button>
+         </form>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="todo-input"
-          id="todoInput"
-          type="text"
-          name="todo-input"
-          placeholder="Create a new todo..."
-          value={todoInput}
-          onChange={handleChange}
-        />
-        <button id="submitNewTodo" type="submit">
-          Add Todo
-        </button>
-      </form>
-    </div>
-  );
+   );
 };
 
 export default TodoForm;
